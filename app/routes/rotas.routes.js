66d27601f50d43efa93c347312e5ab2c5c4b0119
Welson Rosendo Rodrigues <164583871+welson-rodrigues@users.noreply.github.com>
2025-07-23 20:00:39 +0000

    Rotas

--- app/routes/rotas.routes.js
+++ app/routes/rotas.routes.js
@@ -2,29 +2,21 @@ module.exports = app => {
   const Usuari = require("../controllers/user.controllers.js");
   const Lixo = require("../controllers/lixeira.controllers.js");
   const Cidade = require("../controllers/cidade.controllers.js");
-  const Sensor = require("../controllers/sensor.controller.js"); // novo
-  
+  const Sensor = require("../controllers/sensor.controller.js");
+
   var router = require("express").Router();
 
-  // Rotas usuario
-  router.post("/api/user", Usuari.create);
-  //router.get("/api/user", Usuari.findAll);
+  // SUAS ROTAS ORIGINAIS (mantidas intactas)
+  router.post("/api/user", Usuari.create); // Rota original
   router.post("/api/user/login", Usuari.findOne);
-  
-  // Nova rota para verificar email
   router.post("/api/user/check-email", Usuari.checkEmail);
-  
-  // Rota para atualizar senha
   router.put("/api/user/senha", Usuari.updateSenha);
-
-  // Rotas lixeiras
   router.get("/api/lixeira", Lixo.findAll);
   router.put("/api/lixeira/:id", Lixo.update);
   router.get("/api/lixeiras", Sensor.getUltimosDadosSensor);
-  // Outras rotas
-  //router.put("/api/user/:id", Usuari.update);
-  //router.delete("/api/user/:id", Usuari.delete);
-  //router.delete("/api/user", Usuari.deleteAll);
-  
+
+  // MINHA ADIÇÃO (nova rota para cadastro com validação)
+  router.post("/api/user/validated", Usuari.createWithValidation);
+
   app.use("/", router);
 };
